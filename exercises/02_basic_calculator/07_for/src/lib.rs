129c90e180705d7848cfa945b8ee073787456590
Zach Brehm <32712101+zachbrehm@users.noreply.github.com>
2024-09-11 16:56:45 -0400

    Chapter 2 finished
    Completed the remaining exercises for chapter 2.

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,10 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut prod: u32 = 1;
+    for i in 1..=n {
+        prod *= i
+    }
+    prod
 }
 
 #[cfg(test)]
